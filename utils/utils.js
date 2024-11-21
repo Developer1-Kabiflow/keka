ca3f843b21b235fe0607b7865d1cfa38b816a604
Shefeena E S <shefeena.e.s@gmail.com>
2024-11-21 13:42:43 +0400

    Fixed baseURL

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://www.kabiflow.com/api/v1/'
+    ? 'http://www.kabiflow.com/api/v1'
     : 'http://localhost:5000/api/v1';
 
 export default BASE_URL;
