a8a431cdf542bbe7733baeaeb189297994a09d5e
shefeenaes <shefeena.e.s@gmail.com>
2024-12-18 16:23:48 +0400

    url Sharing

--- app/employee/callback/page.js
+++ app/employee/callback/page.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Cookies from "js-cookie";
+import { fetchEmployeeDetails } from "@/app/controllers/employeeController";
 
 export default function Callback() {
   const router = useRouter();
